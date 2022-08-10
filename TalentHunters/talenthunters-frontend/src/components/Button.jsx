a843bc03e5865b5c354c0e48a83c0859614a7737
dalmacsernok <dlmcsernok@gmail.com>
2022-08-10 19:49:54 +0200

    Feature: fetch for get all users

---
+++ TalentHunters/talenthunters-frontend/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ReactComponent({onClick}) {
+  return (
+      <button onClick={onClick} variant="outlined">Get All User </button>
+  );
+}
+
+export default ReactComponent;
--- TalentHunters/talenthunters-frontend/src/pages/Management.js
+++ TalentHunters/talenthunters-frontend/src/pages/Management.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../components/Button';
+import Ptag from '../components/Ptag';
+
 
 const management = () => {
-    return (
-        <p style={{textAlign:"center"}}>Management route for the api endpoints</p>
-        )
+    const [data, setData] = useState([{}, { 'loading': true }]);
+    const handleClick = async () => {
+        const users = await fetchData();
+        console.log(users);
+        setData(users, { 'loading': false })
+    };
+
+
+
+const fetchData = async () => {
+    const response = await fetch('https://localhost:7155/manager/get-users');
+    const data = await response.json();
+    return data;
 }
+
+return (
+    <>
+        <p>Szevasztok</p>
+        <Button onClick={handleClick} />
+        <div>
+            {data.loading ? '' : <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>User Id</th>
+                        <th>Registration Date</th>
+                        <th>Email</th>
+                        <th>Hashed Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((forecast, index) =>
+                        <tr key={forecast.Id}>
+                            <td>{forecast.RegistrationDate}</td>
+                            <td>{forecast.Email}</td>
+                            <td>{forecast.HashedPassword}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>}
+        </div>
+    </>
+
+)
+}
+
 
 export default management;
