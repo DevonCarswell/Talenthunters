f5d30100a83696a69d8007b7247a4e11ede2d3c3
DevonCarswell <kaszazsolt90@gmail.com>
2022-08-24 11:54:36 +0200

    Implementation: frontend routes and pages for division entity

--- TalentHunters/talenthunters-frontend/src/pages/DivisionManagement.js
+++ TalentHunters/talenthunters-frontend/src/pages/DivisionManagement.js
@@ -3,36 +3,48 @@ import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import '../App.css';
 
-
+//TODO employee + divison management with dropdown button or dropdown menu
 const DivisionManagement = () => {
 
+    useEffect(() => {
+        getdivisions();
+    }, [])
+
     const [data, setData] = useState([{}]);
-    const [loading, setLoading] = useState(true);
+    const [employee, setEmployee] = useState([{}]);
+    // dummy data to managers state
+    const [managers, setManagers] = useState([{ firstName: "kamu firstname ", lastName: "kamu lasname" }]);
+    const [loading, setLoading] = useState(false);
     const [divisionId, setDivisionId] = useState("");
-    const inputRef = useRef(null);
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
-    const newemailRef = useRef(null);
+
 
 
     const getdivisions = async () => {
         await fetch(`/division/get-divisions`)
             .then(response => response.json())
             .then(json => setData(json))
-        setLoading(false);
+        setLoading(true)
     };
-    console.log(data);
 
+    
+
+    
     async function getdivision(divisionId) {
-        // const id = inputRef.current.value;
         await fetch(`/division/get-division/${divisionId}`)
             .then(response => response.json())
             .then(json => setData(json))
         setDivisionId("")
-        setLoading(false);
+
 
     }
 
+    async function getEmployeesByDivision(id) {
+    await fetch(`/division/get-employees-by-division/${id}`)
+        .then(response => response.json())
+        .then(json => setEmployee(json))
+
+    };
+   
 
     async function deletedivision(id) {
         await fetch(`/division/delete-division/${id}`,
@@ -43,93 +55,102 @@ const DivisionManagement = () => {
     }
 
 
-    useEffect(() => {
-        getdivisions();
-    }, [])
-
-
-
     return (
         <>
-            <div className="queries">
-                <div>
-                    <label>Get All Divisions</label><br />
-                    <Button onClick={getdivisions} text="Get" /><br />
-                </div>
+            {loading ? (
+                <>
+                    <div className="queries">
+                        <div>
+                            <label>Get All Divisions</label><br />
+                            <Button onClick={getdivisions} text="Get" /><br />
+                        </div>
 
 
-                <div>
-                    <br />
-                    <label>Get single division by id</label> <br />
-                    <input placeholder="Division Id" id="divisionid" value={divisionId} onChange={(e) => setDivisionId(e.target.value)} />
-                    <Button onClick={() => getdivision(divisionId)} text="Search" />
-                </div>
-                <div>
+                        <div>
+                            <br />
+                            <label>Get single division by id</label> <br />
+                            <input placeholder="Division Id" id="divisionid" value={divisionId} onChange={(e) => setDivisionId(e.target.value)} />
+                            <Button onClick={() => getdivision(divisionId)} text="Search" />
+                        </div>
+                        <div>
 
-                    {/*<label>Add New User</label><br />*/}
-                    {/*<input placeholder="Email"*/}
-                    {/*    id="userEmail" ref={emailRef} />-*/}
-                    {/*<input placeholder="Password"*/}
-                    {/*    id="userPassword" type="password" ref={passwordRef} />*/}
-                    <Link to='/adddivision' className="nav-link"> <Button text="Add Division" /></Link>
+                            {/*<label>Add New User</label><br />*/}
+                            {/*<input placeholder="Email"*/}
+                            {/*    id="userEmail" ref={emailRef} />-*/}
+                            {/*<input placeholder="Password"*/}
+                            {/*    id="userPassword" type="password" ref={passwordRef} />*/}
+                            <Link to='/adddivision' className="nav-link"> <Button text="Add Division" /></Link>
 
-                </div>
-
-
-            </div>
-            <div>
-                {data.length >= 1 || loading ?
-                    <table className="table" style={{ textAlign: 'center' }}>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Manager</th>
-                                <th>Employees</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {data.map((division, index) => (
-                                <tr key={index}>
-                                    <td value={division.id}> {division.id} </td>
-                                    <td> {division.name} </td>
-                                    {/*<td>{division.manager}</td>*/}
-                                    {/*<td> {division.Employees} </td>*/}
-                                    {/* TODO employeeRole separation*/}
+                        </div>
 
 
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table> :
-                    <table className="table" style={{ textAlign: 'center' }}>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Manager</th>
-                                <th>Employees</th>
-                                <th>Delete Division</th>
-                            </tr>
-                        </thead>
+                    </div>
 
-                        <tbody>
+                    <div>
 
-                            <tr>
-                                <td value={data.id}> {data.id} </td>
-                                <td>  {data.name} </td>
-                                {/*<td>  {data.Manager} </td>
-                                <td> {data.Employees} </td>*/}
-                                <td value={data.id}>{data.length === 0 ? '' : <Button text='X' onClick={() => deletedivision(data.id)} />}</td>
-                            </tr>
+                        {data.length >= 1 ?
+                            <table className="table" style={{ textAlign: 'center' }}>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Manager</th>
+                                        <th>Employees</th>
+                                    </tr>
+                                </thead>
 
-                        </tbody>
-                    </table>}
-            </div >
+                                <tbody>
+                                    {data.map((division, index) => (
+                                        <tr key={index}>
+                                            <td value={division.id}> {division.id} </td>
+                                            <td> {division.name} </td>
+
+                                            {/* <Link to={{ pathname: `/${this.props.testvalue}`, query: { backUrl } }} />*/}
+
+                                            <td> {division.manager.firstName} </td>
+                                            {/*<td>{division.manager}</td>*/}
+                                            
+<td><Link onClick={getEmployeesByDivision(division.id)} to={{   pathname: `/get-employees-by-division/${division.id}` }}>Employees</Link> </td>
+
+{/* <td><Link to="/division-management" onClick={() => getEmployeesByDivision(division.id)}>Employees</Link> </td> */}
+                                            {/* TODO employeeRole separation*/}
 
 
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table> :
+                            <table className="table" style={{ textAlign: 'center' }}>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Manager</th>
+                                        <th>Employees</th>
+                                        <th>Delete Division</th>
+                                    </tr>
+                                </thead>
 
+                                <tbody>
+
+                                    <tr>
+                                        <td value={data.id}> {data.id} </td>
+                                        <td>  {data.name} </td>
+                                        <td>  {data.manager.firstName} {" "}
+                                            {
+                                                data.manager.lastName
+                                            }
+                                        </td>
+                                        {/*<td> {data.Employees} </td>*/}
+                                        <td value={data.id}>{data.length === 0 ? '' : <Button text='X' onClick={() => deletedivision(data.id)} />}</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>}
+                    </div >
+                </>
+
+            ) : ("")}
         </>
 
     )
